df0bddec01c0dc6d49e0891672cbf805699f8a37
Chris Sison <chris.a.sison@outlook.com>
2020-10-12 00:46:56 -0700

    facebook clone init

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDwYhtPI4misUxnI-JtbaYwChtIKxA6ROg",
+    authDomain: "facebook-clone-fccd7.firebaseapp.com",
+    databaseURL: "https://facebook-clone-fccd7.firebaseio.com",
+    projectId: "facebook-clone-fccd7",
+    storageBucket: "facebook-clone-fccd7.appspot.com",
+    messagingSenderId: "792967774016",
+    appId: "1:792967774016:web:fb80d70db00ae30ad604a5",
+    measurementId: "G-D5PVKZLH9J"
+  };
