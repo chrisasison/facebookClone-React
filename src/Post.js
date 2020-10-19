b67ff67bdcde85ab12d7431c68410d4297fc0a53
Chris Sison <chris.a.sison@outlook.com>
2020-10-19 11:34:45 -0700

    Like, comment, share and more buttons

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp"
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+import NearMeIcon from "@material-ui/icons/NearMe"
+import { ExpandMoreOutlined } from "@material-ui/icons"
 import './Post.css'
 
 function Post({profilePic, image, username, timestamp, message}) {
@@ -20,7 +25,22 @@ function Post({profilePic, image, username, timestamp, message}) {
                 <img src={image} alt="" />
             </div>
             <div className="post__options">
-                <div className="post__option"></div>
+                <div className="post__option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutlineIcon />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircleIcon />
+                    <ExpandMoreOutlined />
+                </div>
             </div>
         </div>
     )
